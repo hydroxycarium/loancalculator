88db90b29126c83bac1b4e5fcebf9b22deac87d1
Jeremy Chelot <hello@hydroxycarium.com>
2019-04-21 20:48:10 +0100

    Complete loancalculator app

---
+++ app.js
@@ -0,0 +1,64 @@
+// Listen to submit
+
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+    document.getElementById('loading').style.display = 'block';
+    document.getElementById('results').style.display = 'none';
+    setTimeout(calculateResults, 2000); 
+    e.preventDefault();
+});
+
+// Calculate Results
+
+function calculateResults() {
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    // Compute monthly payment
+
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+    if (isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+        document.getElementById('results').style.display = 'block';
+        document.getElementById('loading').style.display = 'none';
+    } else {
+        showError('Error');
+    }
+}
+
+function showError(error) {
+    document.getElementById('loading').style.display = 'none';
+    document.getElementById('results').style.display = 'none';
+
+    // Location to insert div
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    // Create a div
+    const errorDiv = document.createElement('div');
+    errorDiv.className = 'alert alert-danger';
+    errorDiv.appendChild(document.createTextNode(error));
+
+    // Insert div
+
+    card.insertBefore(errorDiv, heading);
+
+    // Clear error after 3s
+
+    setTimeout(clearError, 3000);
+}
+
+function clearError() {
+    document.querySelector('.alert').remove();
+}
